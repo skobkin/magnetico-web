70f8fef9a22340c624707847b038d7fa29be5dcf
Alexey Skobkin <skobkin-ru@ya.ru>
2018-06-23 16:59:16 +0300

    Monolog added.

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
