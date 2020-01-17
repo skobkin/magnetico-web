b84900ed2602436dc292dd95ddbb0da15c519cf3
Alexey Skobkin <skobkin-ru@ya.ru>
2020-01-18 01:41:35 +0300

    Entity repositories DI fixed.

--- src/Magnetico/Repository/TorrentRepository.php
+++ src/Magnetico/Repository/TorrentRepository.php
@@ -4,11 +4,11 @@ namespace App\Magnetico\Repository;
 
 use App\Magnetico\Entity\Torrent;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 class TorrentRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Torrent::class);
     }
