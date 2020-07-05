90090061f015c6e7ed340694276778a4402b6ca7
Alexey Skobkin <skobkin-ru@ya.ru>
2020-07-05 04:19:27 +0300

    Implementing simple Redis cache for RSS.

--- src/Api/V1/Controller/RssController.php
+++ src/Api/V1/Controller/RssController.php
@@ -5,16 +5,24 @@ namespace App\Api\V1\Controller;
 use App\Feed\RssGenerator;
 use Symfony\Component\HttpFoundation\{Request, Response};
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class RssController extends AbstractController
 {
     private const CONTENT_TYPE = 'application/rss+xml';
+    private const CACHE_KEY = 'rss_last';
+    private const CACHE_LIFETIME = 600;
 
-    public function last(Request $request, RssGenerator $generator): Response
+    public function last(Request $request, RssGenerator $generator, CacheInterface $magneticodCache): Response
     {
         $page = (int) $request->query->get('page', '1');
 
-        $xml = $generator->generateLast($page);
+        $xml = $magneticodCache->get(self::CACHE_KEY, function (ItemInterface $item) use ($generator, $page) {
+            $item->expiresAfter(self::CACHE_LIFETIME);
+
+            return $generator->generateLast($page);
+        });
 
         return new Response($xml, 200, ['Content-Type' => self::CONTENT_TYPE]);
     }
