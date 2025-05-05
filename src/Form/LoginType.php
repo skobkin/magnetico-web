c803f26e08727c699c064cc5575e2c87c242aa08
Alexey Skobkin <skobkin-ru@ya.ru>
2025-05-05 18:42:05 +0300

    "Rembember me" checkbox added to LoginType form and time period increased to 3 months. (fixes #27, fixes #28)

--- src/Form/LoginType.php
+++ src/Form/LoginType.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\{PasswordType, TextType};
+use Symfony\Component\Form\Extension\Core\Type\{CheckboxType, PasswordType, TextType};
 use Symfony\Component\Form\FormBuilderInterface;
 
 class LoginType extends AbstractType
@@ -14,6 +14,7 @@ class LoginType extends AbstractType
         $builder
             ->add('_username', TextType::class, ['mapped' => false, 'required' => true])
             ->add('_password', PasswordType::class, ['mapped' => false, 'required' => true])
+            ->add('_remember_me', CheckboxType::class, ['mapped' => false, 'required' => false])
         ;
     }
 
